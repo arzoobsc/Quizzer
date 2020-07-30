00f421d36f90e90c0972430ea674087931fd14c7
arzoobsc <arzoobsc@gmail.com>
2020-07-30 21:44:27 +0530

    BookmarkActivity done

--- app/src/main/java/com/ap/quizzer/MainActivity.java
+++ app/src/main/java/com/ap/quizzer/MainActivity.java
@@ -2,6 +2,7 @@
 * video-4 done
 * video-5A done
 * video-6 done
+* video-10 done
 * */
 
 package com.ap.quizzer;
@@ -14,7 +15,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button startBtn;
+    private Button startBtn, bookmarkbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startBtn = findViewById(R.id.start_btn);
+        bookmarkbtn = findViewById(R.id.bookmarks_btn);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent categoryIntent = new Intent(MainActivity.this, CategoriesActivity.class);
                 startActivity(categoryIntent);
+            }
+        });
+
+        bookmarkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bookmarksIntent = new Intent(MainActivity.this, BookmarkActivity.class);
+                startActivity(bookmarksIntent);
             }
         });
     }
